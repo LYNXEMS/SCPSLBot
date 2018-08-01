4992a1d2ab0a66a39982fd21169acc63eccaae54
Unknown <mateuszpietrzakpl@gmail.com>
2018-08-01 14:08:54 +0200

    Cleanup and patreon

--- cogs/patreon.py
+++ cogs/patreon.py
@@ -1,5 +1,5 @@
-from discord.ext import commands
 import discord
+
 
 class Patreon:
     def __init__(self, bot):
@@ -7,13 +7,21 @@ class Patreon:
 
     async def on_member_update(self, before, after):
         server = before.server
-        patroles = [discord.utils.get(server.roles, name="Patreon level - Facility Manager"), discord.utils.get(server.roles, name="Patreon level - Zone Manager"), discord.utils.get(server.roles, name="Patreon level - Major Scientist"), discord.utils.get(server.roles, name="Patreon level - Scientist"), discord.utils.get(server.roles, name="Patreon level - Janitor")]
-        if len(list(set(after.roles).intersection(patroles))) > 0 and discord.utils.get(server.roles, name="Patreon Supporters") not in after.roles:
+        member = before.member
+        patroles = [discord.utils.get(server.roles, name="Patreon level - Facility Manager"),
+                    discord.utils.get(server.roles, name="Patreon level - Zone Manager"),
+                    discord.utils.get(server.roles, name="Patreon level - Major Scientist"),
+                    discord.utils.get(server.roles, name="Patreon level - Scientist"),
+                    discord.utils.get(server.roles, name="Patreon level - Janitor")]
+        if before.roles == after.roles:
+            return
+        elif len(list(set(after.roles).intersection(patroles))) > 0 and discord.utils.get(server.roles, name="Patreon Supporters") not in after.roles:
             await self.bot.add_roles(member, discord.utils.get(server.roles, name="Patreon Supporters"))
         elif len(list(set(after.roles).intersection(patroles))) == 0 and discord.utils.get(server.roles, name="Patreon Supporters") in after.roles:
             await self.bot.remove_roles(member, discord.utils.get(server.roles, name="Patreon Supporters"))
         else:
             return
-            
+
+
 def setup(bot):
     bot.add_cog(Patreon(bot))
