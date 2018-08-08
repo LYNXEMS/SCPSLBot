5fe57bb16a9b61523d8b2feacfbb9e238af31411
Veestire <Veestire@gmail.com>
2018-08-09 00:30:27 +0200

    new commands

--- cogs/ticketsystem.py
+++ cogs/ticketsystem.py
@@ -9,6 +9,7 @@ class ticketSystem:
 		self.bot = bot
 
 
+	#TODO: add cooldown
 
 	@commands.group(pass_context=True)
 	async def ticket(self, ctx):
@@ -100,7 +101,7 @@ class ticketSystem:
 			await self.bot.say("This ticket is already claimed.")
 			return
 
-		ticketids[ticket] = claimer.name
+		ticketids[ticket] = claimer.id
 
 		with open("data/claimedticketids.json", "w") as f:
 			json.dump(ticketids, f)
@@ -139,7 +140,6 @@ class ticketSystem:
 			return
 
 		del tickets[ticket]
-		del ticketids[ticket]
 
 		ticketNumber = tickets["amount_of_open_tickets"] - 1
 		tickets["amount_of_open_tickets"] = ticketNumber
@@ -158,6 +158,93 @@ class ticketSystem:
 
 		await self.bot.say("You have succesfully closed the ticket nr. " + ticket)
 
+	@ticket.command(pass_context=True)
+	@commands.has_role("Tech Support Lead")
+	async def approve(self, ctx):
+		#this is a way to prevent ticket farming, all tickets will have to be approved by the TSL before they get added to the statistics.
+
+		ticket = ctx.message.content[16:]
+
+		if ticket == "":
+			await self.bot.say("Please input a ticket id")
+			return
+
+		with open("data/techsupportstats.json", "r") as f:
+			techstats = json.load(f)
+		with open("data/claimedticketids.json", "r") as f:
+			tickets = json.load(f)
+
+		claimedtech = tickets[ticket]
+
+		if claimedtech == "not_claimed":
+			await self.bot.say("This ticket has not been claimed yet.")
+			return
+
+		techstats[claimedtech] = techstats[claimedtech] + 1
+
+		del tickets[ticket]
+		channel = discord.utils.get(ctx.message.server.channels, name="ticket-nr-{}".format(ticket))
+		await self.bot.delete_channel(channel)
+		await self.bot.send_message(ctx.message.author, "You have approved the ticket. The tech support member will have this ticket added to his/her statistics")
+
+		with open("data/techsupportstats.json", "w") as f:
+			json.dump(techstats, f)
+		with open("data/claimedticketids.json", "w") as f:
+			json.dump(tickets, f)		
+
+	@ticket.command(pass_context=True)
+	async def deny(self,ctx):
+		ticket = ctx.message.content[13:]
+		if ticket == "":
+			await self.bot.say("Please supply a ticket id")
+			return
+		await self.bot.send_message(ctx.message.author, "You have denied the ticket. The tech support member will not have this ticket added to his/her statistics.")
+		channel = discord.utils.get(ctx.message.server.channels, name="ticket-nr-{}".format(ticket))
+		await self.bot.delete_channel(channel)
+
+	@ticket.command(pass_context=True)
+	@commands.has_role("Tech Support")
+	async def info(self,ctx):
+		ticket = ctx.message.content[13:]
+		if ticket == "":
+			await self.bot.say("Please supply a ticket id")
+			return
+		json_data= open("data/tickets.json").read()
+		tickets = json.loads(json_data)
+		embed = discord.Embed(title="Ticket Info", description="Information about the ticket nr. {}".format(ticket), color=0x00ff00)
+		embed.add_field(name="Issuer Name", value=tickets[ticket]["ticket_data"][0][u"issuer_name"], inline=True)
+		embed.add_field(name="Issuer ID", value=tickets[ticket]["ticket_data"][0][u"issuer_id"], inline=True)
+		embed.add_field(name="Ticket ID", value=ticket, inline=True)
+		embed.add_field(name="Ticket Message", value=tickets[ticket]["ticket_data"][0][u"ticket_message"], inline=True)
+		await self.bot.say(embed=embed)
+
+	@ticket.command(pass_context=True)
+	@commands.has_role("Tech Support Lead")
+	async def AddTechSupport(self, ctx):
+		techie = ctx.message.mentions[0].id
+		with open("data/techsupportstats.json", "r") as f:
+			stats = json.load(f)
+		if techie in stats:
+			await self.bot.say("That Tech Support member is already in the statistics")
+			return
+		stats[techie] = 0
+		with open("data/techsupportstats.json", "w") as f:
+			json.dump(stats, f)
+		await self.bot.say("You have succesfully added a new tech support member to the statistics.")
+
+	@ticket.command(pass_context=True)
+	@commands.has_role("Tech Support Lead")
+	async def RemoveTechSupport(self, ctx):
+		techie = ctx.message.mentions[0].id
+		with open("data/techsupportstats.json", "r") as f:
+			stats = json.load(f)
+		if techie not in stats:
+			await self.bot.say("That person is not in the statistics file.")
+			return
+		del stats[techie]
+		with open("data/techsupportstats.json", "w") as f:
+			json.dump(stats, f)
+		await self.bot.say("You have succesfully removed a tech support member from the statistics file")
 
 def setup(bot):
 	bot.add_cog(ticketSystem(bot))
